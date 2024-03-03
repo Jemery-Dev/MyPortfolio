c2004799632684e88bf4cf8a74318fbfaa4022de
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-03-03 23:48:52 +0100

    Update du titre et remove de la musique de fond

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script>
   import './styles.css';
-  import Presentation from './pages/Presentation.svelte'
+  import  Presentation  from './pages/Presentation.svelte'
   import Competence from './pages/Competence.svelte'
   import Projet from './pages/Projet.svelte'
  
-  function jouerMusique(){
+  /*function jouerMusique(){
       var audio = new Audio('enmodefeuj.mp3');
       audio.play();
-    }
+    }*/
 </script>
 <html class="dark" lang="fr">
-  <button on:click={jouerMusique()}>f</button>
+  <!-- <button on:click={jouerMusique()}>f</button>-->
   <Presentation />
   <Competence />
   <Projet />
 </html>
+
