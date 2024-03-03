01a9d9ee6a2911f5469a9566822bfff8306ac661
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-03-03 23:28:02 +0100

    Revert "Modification mineur"
    This reverts commit db35d8db6b07957147e25c2a378d03272cc66f0f.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,14 @@
   import Competence from './pages/Competence.svelte'
   import Projet from './pages/Projet.svelte'
  
-  /*function jouerMusique(){
+  function jouerMusique(){
       var audio = new Audio('enmodefeuj.mp3');
       audio.play();
-    }*/
+    }
 </script>
 <html class="dark" lang="fr">
-  <!-- <button on:click={jouerMusique()}>f</button>-->
+  <button on:click={jouerMusique()}>f</button>
   <Presentation />
   <Competence />
   <Projet />
 </html>
-
--- src/routes/pages/Competence.svelte
+++ src/routes/pages/Competence.svelte
@@ -31,25 +31,23 @@
       <Button pill size="xl" on:click={() => choix = "Back-End"} color="alternative">Back-End</Button>
       <Search size="lg" placeholder="Rechercher compétence" on:input={updateSearchText}></Search>
     </div>
-    
+  
+  
+
+
     
     <div id="gallery" class="flex flex-wrap justify-center space-y-4">
       <Card class="hidden"/>
       {#each skills as skill (skill.alt)}
       {#if skill.alt && (skill.alt.toLowerCase().includes(searchTexte.toLowerCase()) || searchTexte === "")}
         {#if skill.category && (skill.category.includes(choix) || choix === "all")}
-            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64 front__card">
+            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64">
               <img src={skill.src} alt={skill.alt} class="w-24 h-24 mx-auto mb-3" />
               <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">{skill.alt}</h5>
               <p class="mb-2 text-sm font-normal text-gray-700 dark:text-gray-400">{montrerCategory(skill.category)}</p>
               <Button class="text-sm" href={skill.href}> 
                 En savoir plus <ArrowRightOutline class="w-3 h-3 ms-1 text-white" />
               </Button>
-            </Card>
-
-            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64 back__card">
-              <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">{skill.alt}</h5>
-              <p class="mb-2 text-sm font-normal text-gray-700 dark:text-gray-400">{montrerCategory(skill.category)}</p>
             </Card>
           {/if}
         {/if}
