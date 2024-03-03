4a03b71e7cb524412756746887f2dbbbbc277884
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-03-03 23:15:33 +0100

    Ajout de popupp sur les skills et mise à jour de l'interface

--- src/routes/pages/Competence.svelte
+++ src/routes/pages/Competence.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {Card, Button, Search} from 'flowbite-svelte';
+    import {Card, Button, Search, Popover} from 'flowbite-svelte';
     import {ArrowRightOutline } from 'flowbite-svelte-icons';
     import skills from '../../data/skills.json';
   
@@ -7,6 +7,9 @@
     let choix = "all"; // Valeur pour les boutons de catégorie
     let searchTexte = ""; // Valeur pour la search bar
   
+    /**
+	 * @param {string[]} liste
+	 */
     function montrerCategory(liste){
     let texte = "";
     for (const categorie of liste) {
@@ -15,20 +18,22 @@
     return texte.trim();
   }
   
+    /**
+	 * @param {{ target: { value: string; }; }} event
+	 */
     function updateSearchText(event) {
       searchTexte = event.target.value;
     }
   
 </script>
 
+
+
 <section id="competences" class="p-8">
   <div class="flex items-center justify-center text-4xl text-gray-100 font-black">
       Compétences
   </div>
     <div class="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3 mb-3 mx-auto">
-      <Button pill size="xl" on:click={() => choix = "all"} outline>Tous</Button>
-      <Button pill size="xl" on:click={() => choix = "Front-End"} color="alternative">Front-End</Button>
-      <Button pill size="xl" on:click={() => choix = "Back-End"} color="alternative">Back-End</Button>
       <Search size="lg" placeholder="Rechercher compétence" on:input={updateSearchText}></Search>
     </div>
     
@@ -38,21 +43,24 @@
       {#each skills as skill (skill.alt)}
       {#if skill.alt && (skill.alt.toLowerCase().includes(searchTexte.toLowerCase()) || searchTexte === "")}
         {#if skill.category && (skill.category.includes(choix) || choix === "all")}
-            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64 front__card">
+            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64">
               <img src={skill.src} alt={skill.alt} class="w-24 h-24 mx-auto mb-3" />
               <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">{skill.alt}</h5>
               <p class="mb-2 text-sm font-normal text-gray-700 dark:text-gray-400">{montrerCategory(skill.category)}</p>
-              <Button class="text-sm" href={skill.href}> 
-                En savoir plus <ArrowRightOutline class="w-3 h-3 ms-1 text-white" />
-              </Button>
-            </Card>
-
-            <Card class="max-w-xs mx-auto w-full md:w-1/2 lg:w-1/4 h-64 back__card">
-              <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">{skill.alt}</h5>
-              <p class="mb-2 text-sm font-normal text-gray-700 dark:text-gray-400">{montrerCategory(skill.category)}</p>
+              {#if skill.alt === "C++"}
+                <Button class="text-sm" id="lecplusplus" href={skill.href}> 
+                    En savoir plus <ArrowRightOutline class="w-3 h-3 ms-1 text-white" />
+                </Button>
+                <Popover class="w-64 text-base font-light" title="{skill.alt}" triggeredBy="#lecplusplus">{skill.text}</Popover>
+              {:else}
+                <Button class="text-sm" id="{skill.alt}-id" href={skill.href}> En savoir plus <ArrowRightOutline class="w-3 h-3 ms-1 text-white" />
+                </Button>
+                <Popover class="w-64 text-base font-light" title="{skill.alt}" triggeredBy="#{skill.alt}-id">{skill.text}</Popover>
+              {/if}
             </Card>
           {/if}
         {/if}
       {/each}
       </div>
   </section>
+
