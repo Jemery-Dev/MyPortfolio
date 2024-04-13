40c0254578e832630b6270abd7aead7a64ed9333
Jérémy Girard <oqaislenning@gmail.com>
2024-04-13 15:33:16 +0200

    Ajout de intersectionObserver

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
   import Presentation from './pages/Presentation.svelte';
   import Competence from './pages/Competence.svelte';
   import Projet from './pages/Projet.svelte';
@@ -6,6 +8,20 @@
   import Header from './Header.svelte';
 	import { onMount } from 'svelte';
   import './styles.css';
+
+  const startAnimation = (entries, observer) => {
+      entries.forEach(entry => {
+        entry.target.classList.toggle("slide-in-from-right", entry.isIntersecting);
+      });
+    };
+    
+    let observer = new IntersectionObserver(startAnimation);
+    const options = { root: null, rootMargin: '0px', threshold: 1 }; 
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(el => {
+      observer.observe(el, options);
+    });
 
 
 </script>
