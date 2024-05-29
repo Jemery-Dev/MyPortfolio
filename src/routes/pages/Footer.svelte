4cf8f08458688da34a751ca2f37be4972aade031
Jérémy Girard <75862623+Jemery-Dev@users.noreply.github.com>
2024-05-29 09:19:37 +0200

    Add Footer

--- src/routes/pages/Footer.svelte
+++ src/routes/pages/Footer.svelte
@@ -1,22 +1,48 @@
-<footer class="bg-yellow-100 dark:bg-gray-900 dark:text-white text-slate-800 py-12">
-    <div class="lg:max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
-        <div class="grid sm:grid-cols-1 lg:grid-cols-2 gap-24">
-            <div>
-                <h3 class="text-xl font-bold mb-4">Me contacter</h3>
-                <ul class="space-y-3">
-                    <li><a href="mailto:jeremy.girard015@gmail.com" class="text-base">jeremy.girard015@gmail.com</a></li>
-                    <li><a href="tel:+1234567890" class="text-base">+33 7 78 66 28 66</a></li>
-                </ul>
-            </div>
-            <div>
-                <h3 class="text-xl font-bold mb-4">Me suivre</h3>
-                <ul class="flex space-x-4">
-                    <li><a href="https://github.com/Jemery-Dev" class="text-base">GitHub</a></li>
-                    <li><a href="https://twitter.com/JeremyPujadas" class="text-base">Twitter</a></li>
-                    <li><a href="https://www.linkedin.com/in/jérémy-girard-b554b6251/" class="text-base">LinkedIn</a></li>
-                    <li><a href="https://www.instagram.com/jemery_girard/" class="text-base">Instagram</a></li>
-                </ul>
-            </div>
+<script>
+    import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, FooterBrand, FooterIcon } from 'flowbite-svelte';
+    import { FacebookSolid, GithubSolid, DiscordSolid, TwitterSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Footer footerType="socialmedia" class="bg-darker-beige dark:bg-slate-900">
+    <div class="md:flex md:justify-between">
+      <div class="mb-6 md:mb-0">
+        <FooterBrand href="https://flowbite.com" src="/favicon.jpg" alt="Portfolio Logo" name="Jérémy Girard" />
+      </div>
+      <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+        <div>
+          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Ressources</h2>
+          <FooterLinkGroup>
+            <FooterLink liClass="mb-4" href="https://svelte.dev" target="_blank">Svelte</FooterLink>
+            <FooterLink liClass="mb-4" href="https://flowbite-svelte.com" target="_blank">Flowbite</FooterLink>
+          </FooterLinkGroup>
         </div>
+        <div>
+          <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Réseau</h2>
+          <FooterLinkGroup>
+            <FooterLink liClass="mb-4" href="/">GitHub</FooterLink>
+            <FooterLink liClass="mb-4" href="/">Discord</FooterLink>
+          </FooterLinkGroup>
+        </div>
+      </div>
     </div>
-</footer>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <div class="sm:flex sm:items-center sm:justify-between">
+      <FooterCopyright href="/" by="Jérémy Girard" />
+      <div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center sm:mt-0">
+        <FooterIcon href="/">
+          <FacebookSolid class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </FooterIcon>
+        <FooterIcon href="/">
+          <DiscordSolid class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </FooterIcon>
+        <FooterIcon href="/">
+          <TwitterSolid class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </FooterIcon>
+        <FooterIcon href="/">
+          <GithubSolid class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </FooterIcon>
+        <FooterIcon href="/">
+        </FooterIcon>
+      </div>
+    </div>
+  </Footer>
